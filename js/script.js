b759bc7db65a4a28b9e79f1d9c0ff09852367215
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2024-03-18 21:22:19 +0530

    Updated JS with elements: Scroll and Nav

--- js/script.js
+++ js/script.js
@@ -83,13 +83,13 @@ const obs = new IntersectionObserver(
 );
 obs.observe(sectionHeroEl);
 
-$(window).on("scroll", function() {
-  if ($(window).scrollTop() > $(".section-hero").outerHeight()) {
-    $(".header").addClass("sticky");
-  } else {
-    $(".header").removeClass("sticky");
-  }
-});
+// $(window).on("scroll", function() {
+//   if ($(window).scrollTop() > $(".section-hero").outerHeight()) {
+//     $(".header").addClass("sticky");
+//   } else {
+//     $(".header").removeClass("sticky");
+//   }
+// });
 
 
 ///////////////////////////////////////////////////////////
@@ -168,11 +168,11 @@ const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute('aria-expanded');
-  
+
   for (i = 0; i < items.length; i++) {
     items[i].setAttribute('aria-expanded', 'false');
   }
-  
+
   if (itemToggle == 'false') {
     this.setAttribute('aria-expanded', 'true');
   }
